e48be4dbb2c9a7fd3c3c797509a61abbad91d5a5
astragenius <darthmalak86@gmail.com>
2022-03-21 12:25:15 +0100

    add - Added computerChoice function to return randomly 'rock, paper, scissors'

---
+++ rock-paper-scissors/app.js
@@ -0,0 +1,20 @@
+
+// a comptuerChoice fuction that gives randomly "rock, paper, siccors" back.
+
+function computerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let randomInt = Math.floor(Math.random() * 3);
+    console.log(choices[randomInt]); 
+}
+
+// a playerChoice function that ask the Player with prompt to choose between "rock, paper, scissors"
+
+
+// a playround function thats plays one round of the game.
+
+
+
+//a function that checks how has won the game.
+
+
+computerChoice();
