d16f773c8ca49d4cf33c7d49fefb5f74f8538751
astragenius <darthmalak86@gmail.com>
2022-03-21 13:19:34 +0100

    add - Added checkWin function to check wich player has won the game

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -3,8 +3,9 @@
 
 function computerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
-    let randomInt = Math.floor(Math.random() * 3);
-    return choices[randomInt];
+    let cChoice = choices[Math.floor(Math.random() * 3)];
+    console.log(cChoice);
+    return cChoice;
 }
 
 // a playerChoice function that ask the Player with prompt to choose between "rock, paper, scissors"
@@ -21,5 +22,42 @@ function playerChoice() {
 
 //a function that checks how has won the game.
 
+const player = playerChoice();
+const computer = computerChoice();
 
-console.log(playerChoice());
+
+function checkWinner(pChoice, cChoice) {
+
+    if(pChoice === 'rock' && cChoice === 'paper') {
+
+        return `Computer wins with ${cChoice}`; 
+
+    }if(pChoice === 'rock' && cChoice === 'scissors') {
+
+        return `Player wins with ${pChoice}`; 
+
+    }if(pChoice === 'paper' && cChoice === 'rock') {
+
+        return `Player wins with ${pChoice}`; 
+
+    }if(pChoice === 'paper' && cChoice === 'scissors') {
+
+        return `computer wins with ${cChoice}`; 
+
+    }if(pChoice === 'scissors' && cChoice === 'paper') {
+
+        return `Player wins with ${pChoice}`;
+
+    }if(pChoice === 'scissors' && cChoice === 'rock') {
+
+        return `computer wins with ${cChoice}`;
+    }else {
+        return `Its a draw! Player: ${pChoice} Computer: ${cChoice}`;
+    }
+    
+}
+
+console.log(checkWinner(player, computer));
+
+
+
