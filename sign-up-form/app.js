663d15e84e15329cc61edeb9ef76a25c5ff9fc08
astragenius <darthmalak86@gmail.com>
2022-04-26 21:59:25 +0200

    add - password matcher

--- sign-up-form/app.js
+++ sign-up-form/app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.myForm');
 form.noValidate = true;
-const email = document.getElementById('email');
-console.log(email.parentElement)
+
+
 
 
 
@@ -9,7 +9,10 @@ form.addEventListener('submit', validateForm);
 
 
 function validateForm(e) {
-
+    
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirm_password');
     const form = e.target;
 
     if(form.checkValidity()) {
@@ -18,7 +21,7 @@ function validateForm(e) {
         e.preventDefault()
         if(email.checkValidity()) {
             email.parentElement.classList.remove('error-msg')
-        }
+        }   
         else {
             email.parentElement.classList.add('error-msg')
         }
