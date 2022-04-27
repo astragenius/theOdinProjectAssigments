98d28b6c3fa02f8afe7d204edd439fedafb5c4fe
astragenius <darthmalak86@gmail.com>
2022-04-27 19:43:51 +0200

    del - delete validateform code - to rewrite it

--- sign-up-form/app.js
+++ sign-up-form/app.js
@@ -10,21 +10,4 @@ form.addEventListener('submit', validateForm);
 
 function validateForm(e) {
     
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const confirmPassword = document.getElementById('confirm_password');
-    const form = e.target;
-
-    if(form.checkValidity()) {
-
-    }else {
-        e.preventDefault()
-        if(email.checkValidity()) {
-            email.parentElement.classList.remove('error-msg')
-        }   
-        else {
-            email.parentElement.classList.add('error-msg')
-        }
-
-    }
 };
